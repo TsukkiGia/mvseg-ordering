d41dc1dbd74f3c891781d6b15bbaf9f05c39f415
Gianna Torpey <gianna.torpey@gmail.com>
2026-01-06 09:26:43 -0500

    Added encoder configs

--- experiments/ordering/__init__.py
+++ experiments/ordering/__init__.py
@@ -1,6 +1,7 @@
 from .base import OrderingConfig, compute_shard_indices, AdaptiveOrderingConfig, NonAdaptiveOrderingConfig
 from .random import RandomConfig
 from .mse_proximity import MSEProximityConfig
+from .representative import RepresentativeConfig
 from .uncertainty import UncertaintyConfig
 
 __all__ = [
@@ -10,5 +11,6 @@ __all__ = [
     "compute_shard_indices",
     "RandomConfig",
     "MSEProximityConfig",
+    "RepresentativeConfig",
     "UncertaintyConfig",
 ]
